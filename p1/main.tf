e8da92a659884ad386da4aaf9624f6827fc28f6d
Walter Vargas <w@walter.bio>
2023-10-20 21:53:44 +0200

    add external data source

--- p1/main.tf
+++ p1/main.tf
@@ -12,3 +12,11 @@ provider "random" {}
 resource "random_pet" "name" {
   length = 10
 }
+
+data "external" "example" {
+  program = ["ps aux"]
+}
+
+output "custom_output" {
+  value = data.external.example.result.custom_value
+}
