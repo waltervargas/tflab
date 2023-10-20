6f75b0db8dc78e79f762d619cabee3126c577dd7
Walter Vargas <w@walter.bio>
2023-10-20 21:19:15 +0200

    add p1

---
+++ p1/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {
+  length = 10
+}
